83f150872893b9b2123a04f1692f42e8e2e6824c
Raul <rlopgar599@g.educaand.es>
2024-11-26 09:37:51 +0100

    Proyecto inicial con clases Coche y Cliente

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Coche coche = new Coche("Toyota", "Corolla", 2020);
+        Cliente cliente = new Cliente("Juan Pérez", "123456789");
+        System.out.println(coche);
+        System.out.println(cliente);
+    }
+}
